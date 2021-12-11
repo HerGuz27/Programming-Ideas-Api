b077642be29909380443b77a5890b1ff3ea6d5c2
Hernan <hernangut96@gmail.com>
2021-12-11 00:39:12 -0300

    conexion con database heroku exitosa

--- index.js
+++ index.js
@@ -13,35 +13,30 @@ app.get("/",(req,res)=>{
     res.send("Hola Mundo")
 })
 app.get("/datos",(req,res)=>{
-const mysql = require('mysql2');
-const conexion= mysql.createConnection({
-    host : 'ec2-3-211-228-251.compute-1.amazonaws.com',
-    database : 'dcrvpthkpb2k2e',
-    user : 'nzbzmmeptypctl',
-    port:'5432',    
-    password : '8d56c291bff0e6794687455c2c2e4a8ae219cb2a77fa0b5229764fec7aca4c6a',
-});
-
-
-conexion.connect(function(err) {
-    if (err) {
-        console.error('Error de conexion: ' + err.stack);
-        return;
-    }
-    console.log('Conectado con el identificador ' + conexion.threadId);
-});
-
-conexion.query('SELECT * FROM Persons', function (error, results, fields) {
-     if(error) {
-      res.status = 422
-      res.json(error)
-      return // without this return the error can happen.
-    }
-    return res.json(results)
-  })
-
-conexion.end();
+    const { Client } = require('pg')
+    const connectionData = {
+        user: 'nzbzmmeptypctl',
+        host: 'ec2-3-211-228-251.compute-1.amazonaws.com',
+        database: 'dcrvpthkpb2k2e',
+        password: '8d56c291bff0e6794687455c2c2e4a8ae219cb2a77fa0b5229764fec7aca4c6a',
+        port: 5432,
+        ssl:{
+            rejectUnauthorized:false,
+        },
+      }
+      const conexion=new Client(connectionData)
+conexion.connect()
+conexion.query('SELECT * FROM Persons')
+    .then(response => {
+        console.log(response.rows)
+        return res.json(response.rows)
+        
+    })
+    .catch(err => {
+        conexion.end()
+    })
 })
+
 app.listen(port,()=>
 console.log('Example app listening at http://localhost:'+port)
 )
